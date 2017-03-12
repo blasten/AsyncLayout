5908cee048d194d1322fb4c04ae9eb688b3efc14
Emmanuel Garcia <egarciad@google.com>
2017-03-12 14:57:11 -0700

    A few renames

--- src/Recycler.js
+++ src/Recycler.js
@@ -30,21 +30,20 @@ export default class Recycler {
     this._pool = pool;
     this._nodes = [];
     this._queue = [];
-    this._keptNodes = {};
-    this._startIdx = UNKNOWN_IDX;
+    this._keyNodeMap = {};
   }
 
   refresh(nodes) {
-    let resetSet = [];
+    let resetSet = [], startIdx = UNKNOWN_IDX;
     nodes.forEach((node, idx) => {
       let key = node.dataset.key;
-      if (!this._keptNodes[key]) {
+      if (!this._keyNodeMap[key]) {
         resetSet.push(key);
         this._keepNode(node);
       }
     });
     if (nodes === this._nodes) {
-      this._startIdx = this.startMeta.idx;
+      startIdx = this.startMeta.idx;
       this._nodes = [];
     }
     this
@@ -54,31 +53,31 @@ export default class Recycler {
         this.$isClientIncomplete,
         Math.max(MIN_BATCH_SIZE, nodes.length),
         true,
+        startIdx
       )
       .then(_ => {
         resetSet.forEach(key => {
-          let meta = this._meta.get(this._keptNodes[key]);
+          let meta = this._meta.get(this._keyNodeMap[key]);
           if (!meta || this.has(meta.idx)) {
             this.release(key);
           }
         });
-        this._startIdx = UNKNOWN_IDX;
       });
   }
 
   recycle(isBuffer, recalcs) {
     let batchSize = isBuffer ? 3 : MIN_BATCH_SIZE,
-      condition = isBuffer
+      isJobDone = isBuffer
         ? this.$isBufferIncomplete
         : this.$isClientIncomplete,
       waiter = isBuffer ? forIdleTime : forNextTick;
     return this
-      ._schedule(RENDER_START, waiter, condition, batchSize, recalcs)
+      ._schedule(RENDER_START, waiter, isJobDone, batchSize, recalcs)
       .then(_ =>
-        this._schedule(RENDER_END, waiter, condition, batchSize, recalcs));
+        this._schedule(RENDER_END, waiter, isJobDone, batchSize, recalcs));
   }
 
-  _schedule(dir, awaitFor, isDone, jobSize, recalcs) {
+  _schedule(dir, awaitFor, isDone, jobSize, recalcs, startIdx = UNKNOWN_IDX) {
     return addJob(
       {
         _waiter: awaitFor,
@@ -87,7 +86,7 @@ export default class Recycler {
           if (isDone(this.startMeta, this.endMeta, dir)) {
             return;
           }
-          let updateTask = this._updateTree(dir, jobSize);
+          let updateTask = this._updateTree(dir, jobSize, startIdx);
           if (updateTask.size === 0) {
             return;
           }
@@ -112,12 +111,12 @@ export default class Recycler {
     if (!dataset.pool) {
       dataset.pool = DEFAULT_POOL_ID;
     }
-    if (!this._keptNodes[dataset.key]) {
+    if (!this._keyNodeMap[dataset.key]) {
       pushToPool(this._pool, dataset.pool, hide(node));
     }
   }
 
-  _updateTree(dir, increment) {
+  _updateTree(dir, increment, startIdx) {
     let node,
       meta,
       nodes = this._nodes,
@@ -152,7 +151,7 @@ export default class Recycler {
       nodes.shift();
     }
     // Dequeue node or allocate a new one.
-    while (size < increment && (node = this._getNode(dir))) {
+    while (size < increment && (node = this._getNode(dir, startIdx))) {
       dir == RENDER_START ? nodes.unshift(node) : nodes.push(node);
       this._append(node);
       size++;
@@ -198,10 +197,10 @@ export default class Recycler {
     }
   }
 
-  _getNode(dir) {
+  _getNode(dir, startIdx = UNKNOWN_IDX) {
     let idx, size = this.$size();
     if (this._nodes.length == 0) {
-      idx = Math.min(this._startIdx, size - 1);
+      idx = Math.min(startIdx, size - 1);
     } else {
       idx = dir == RENDER_START ? this.startMeta.idx - 1 : this.endMeta.idx + 1;
       if (idx < 0) {
@@ -212,7 +211,7 @@ export default class Recycler {
   }
 
   _getNodeByIdx(idx) {
-    let meta, node, key, pool, keptNodes = this._keptNodes, size = this.$size();
+    let meta, node, key, pool, keptNodes = this._keyNodeMap, size = this.$size();
 
     meta = this.$initMeta(this._storage[idx] || { idx, key: idx });
 
@@ -246,12 +245,12 @@ export default class Recycler {
       node.dataset.key != '',
       'Node should contain a `dataset.key` property',
     );
-    this._keptNodes[node.dataset.key] = node;
+    this._keyNodeMap[node.dataset.key] = node;
   }
 
   keep(idx) {
     let node, meta = this._storage[idx];
-    if (meta && (node = this._keptNodes[meta.key])) {
+    if (meta && (node = this._keyNodeMap[meta.key])) {
       this.$layout(node, meta);
       return meta.key;
     }
@@ -265,13 +264,13 @@ export default class Recycler {
   }
 
   release(key) {
-    let node = this._keptNodes[key];
+    let node = this._keyNodeMap[key];
     if (!node) {
       return;
     }
     let meta = this._meta.get(node) || EMPTY;
 
-    delete this._keptNodes[key];
+    delete this._keyNodeMap[key];
 
     if (this._nodes[meta.idx - this.startMeta.idx] == node) {
       this.$layout(node, meta);
